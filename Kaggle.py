48655606abad5c8c195941aecb8b021c79e12c37
DarrenTeo <DarrenTeo001@gmail.com>
2023-04-30 10:55:35 +0800

    Update Kaggle.py

--- Kaggle.py
+++ Kaggle.py
@@ -4,9 +4,11 @@ import plotly.express as px
 
 st.set_page_config(layout="wide")
 
+st.header('Kaggle Survey Analysis')
+
 @st.cache()
 def load_data():
-    df = pd.read_csv(r'https://raw.githubusercontent.com/DarrenTeo/Kaggle_Survey/main/kaggle_survey_2017_2021.csv', sep=',',skiprows=1)
+    df = pd.read_csv('https://raw.githubusercontent.com/DarrenTeo/Kaggle_Survey/main/kaggle_survey_2017_2021.csv', sep=',',skiprows=1)
     df['survey_id'] = df.index
     
     ################
