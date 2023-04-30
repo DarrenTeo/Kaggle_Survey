26025ab6888c9ab86a606f5c24fbc4276224333a
DarrenTeo <DarrenTeo001@gmail.com>
2023-04-30 11:01:43 +0800

    Update Kaggle.py

--- Kaggle.py
+++ Kaggle.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 
 st.header('Kaggle Survey Analysis')
 
-@st.cache_data
+
 def load_data():
     df = pd.read_csv(r'kaggle_survey_2017_2021.csv', sep=',',skiprows=1)
     df['survey_id'] = df.index
